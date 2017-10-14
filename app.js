8d69efcfbd34464c63ff34cd972bbda432691112
kr3110 <n.nkr3110@gmail.com>
2017-10-14 16:28:27 -0400

    swap listen to get :v

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.listen('/',function(req,res){
+app.get('/',function(req,res){
   res.send('hello world');
 });
 
-
-app.listen(8080);
+var port =  process.env.PORT || 8080;
+app.listen(port);
