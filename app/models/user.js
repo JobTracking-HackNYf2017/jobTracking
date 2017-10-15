90fe8797172ea09a8b9783f1d67dc22759f0336d
kr3110 <n.nkr3110@gmail.com>
2017-10-15 09:30:39 -0400

    almost there

--- app/models/user.js
+++ app/models/user.js
@@ -1,30 +1,46 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 //user  schema
+
+var jobSchema = new Schema({
+  Subject: {
+    type: String,
+    required: true
+  },
+  introEmail: {
+    type: String,
+      required: true
+  },
+  company: {
+    type: String,
+      required: true
+  }
+});
 var userSchema = new Schema({
-    _id:{
-        type: String
-        , required: true
-    }
-    , token: {
-        type: String
-        , required: true
-    }
-    , name: {
-        type: String
-        , required: true
-    }
-    , email: {
-        type: String
-        , required: true
-    }
+  _id: {
+    type: String,
+    required: true
+  },
+  token: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  job: [jobSchema]
 });
 // generating a hash
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+  return bcrypt.compareSync(password, this.local.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
--- passport.js
+++ passport.js
@@ -5,95 +5,114 @@ var gmailAPI = require('node-gmail-api');
 var base64 = require('base-64');
 var _ = require('underscore');
 
+var appli;
 module.exports = function(passport) {
 
-// used to serialize the user for the session
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-// used to deserialize the user
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
+appli=[];
+  // used to serialize the user for the session
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
   });
-});
+
+  // used to deserialize the user
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+  //HardCode Companies
+  //in a future will be awesome to extract company with AI
+
+  var file = require('./companies.json');
+
+  var companies = file.companies;
 
 
+  // code for login (use('local-login', new LocalStategy))
+  // code for signup (use('local-signup', new LocalStategy))
+  // code for facebook (use('facebook', new FacebookStrategy))
+  // code for twitter (use('twitter', new TwitterStrategy))
 
-
-// code for login (use('local-login', new LocalStategy))
-// code for signup (use('local-signup', new LocalStategy))
-// code for facebook (use('facebook', new FacebookStrategy))
-// code for twitter (use('twitter', new TwitterStrategy))
-
-// =========================================================================
-// GOOGLE ==================================================================
-// =========================================================================
-passport.use(new GoogleStrategy({
+  // =========================================================================
+  // GOOGLE ==================================================================
+  // =========================================================================
+  passport.use(new GoogleStrategy({
 
       clientID: config.clientID,
       clientSecret: config.clientSecret,
       callbackURL: config.callback,
-      },
-      function(token, refreshToken, profile, done) {
+    },
+    function(token, refreshToken, profile, done) {
+
+
 
       // make the code asynchronous
       // User.findOne won't fire until we have all our data back from Google
       process.nextTick(function() {
+
         var email = new gmailAPI(token);
-      var messages = email.messages('label:inbox "Thank you for your interest" newer_than:1y', {max: 10,format:'full'});
-
-        messages.on('data', function (d) {
-          var obj = _.findWhere(d.payload.headers, {name: 'Subject'});
-          console.log(obj.value);
-          var text = d.snippet;
-          console.log('text ')
-          console.log(text);
-          })
+        var messages = email.messages('label:inbox "Thank you for your interest" newer_than:1y', {
+          max: 10,
+          format: 'full'
+        });
+        messages.on('data', function(d) {
+          for (var i = 0; i < companies.length; i++) {
+            if (d.snippet.includes(companies[i])) {
+              var obj = _.findWhere(d.payload.headers, {
+                name: 'Subject'
+              });
+              var singleApplication = {
+                Subject : obj,
+                introEmail : d.snippet,
+                company : companies[i],
+              };
+              appli.push(singleApplication);
+              i=companies.length+1;
+            }
+          }
+        });
         // check if the user is already logged in
-        console.log(profile);
-          User.findOne({
-            '_id': profile.id
-          }, function(err, user) {
-            if (err)
-              return done(err);
+        User.findOne({
+          '_id': profile.id
+        }, function(err, user) {
+          if (err)
+            return done(err);
 
-            if (user) {
-              // if there is a user id already but no token (user was linked at one point and then removed)
-              if (!user.token) {
-                user.token = token;
-                user.name = profile.displayName;
-                user.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
+          if (user) {
+            // if there is a user id already but no token (user was linked at one point and then removed)
+            if (!user.token) {
 
-                user.save(function(err) {
-                  if (err)
-                    return done(err);
-
-                  return done(null, user);
-                });
-              }
-
-              return done(null, user);
-            } else {
-              var newUser = new User();
-
-              newUser._id = profile.id;
-              newUser.token = token;
-              newUser.name = profile.displayName;
-              newUser.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
-
-              newUser.save(function(err) {
+              user.token = token;
+              user.name = profile.displayName;
+              user.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
+              user.job = appli;
+              user.save(function(err) {
                 if (err)
                   return done(err);
 
-                return done(null, newUser);
+                return done(null, user);
               });
             }
-          });
+
+            return done(null, user);
+          } else {
+            var newUser = new User();
+            newUser._id = profile.id;
+            newUser.token = token;
+            newUser.name = profile.displayName;
+            newUser.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
+            newUser.job=appli;
+            newUser.save(function(err) {
+              if (err)
+                return done(err);
+
+              return done(null, newUser);
+            });
+          }
+        });
 
       });
 
-}));
+    }));
 
 };
